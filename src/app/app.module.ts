3c392b81ad74e78597d6c3bc7b225e3b48e82b3c
Jorge <josando210182@gmail.com>
2018-02-04 19:05:41 +0100

    Subiendo Heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'; // NgModel lives here
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HeroeComponent } from './heroe/heroe.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
     HeroeComponent
   ],
   imports: [
--- src/app/heroe/heroe.component.ts
+++ src/app/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroe',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
+  hero: Hero = {
+    name: 'Windstorm',
+    id: 1
+  };
 
   constructor() { }
 
